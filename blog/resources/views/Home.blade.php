2ecd5826a32ca65dbbaa5071812634ffd65f53f9
Pranto-275 <67626492+Pranto-275@users.noreply.github.com>
2021-08-05 20:20:30 +0600

    File_upload_complete
    MB percentage, remaining info

--- blog/resources/views/Home.blade.php
+++ blog/resources/views/Home.blade.php
@@ -10,6 +10,14 @@
                <div class="card-body">
                     <input type="file" id="FileID" class="form-control">
                     <button onClick="onUpload()" id="UploadBtnID" class="btn btn-primary btn-block my-3"> Upload</button>
+
+                    <table border="1" width="100%">
+                        <tr>
+                            <td> <h6 id="uploadpercentageid">00%</h6></td>
+                            <td> <h6 id="uploadMBid">00</h6></td>
+                            <td> <h6 id="uploadperMBid">00</h6></td>
+                        </tr>
+                    </table>
                </div>
            </div>
         </div>
@@ -22,8 +30,8 @@
 @section('script')
 <script>
     function onUpload(){
-        let spinnerLod = "<div class='spinner-border text-white shadow-sm' role='status'></div>"
-        $('#UploadBtnID').html(spinnerLod)
+        // let spinnerLod = "<div class='spinner-border text-white shadow-sm' role='status'></div>"
+        // $('#UploadBtnID').html(spinnerLod)
        let myfile = document.getElementById('FileID').files[0];
     //    let myfilename = myfile.name;
     //    let myfilesize = myfile.size;
@@ -34,7 +42,19 @@
         let FileData = new FormData;
         FileData.append('Filekey',myfile);
 
-        let config = {header:{'content-type':'multipart/formdata'}}
+        let config = {
+        headers:{'content-type':'multipart/formdata'},
+        onUploadProgress: function(progressEvent){
+            let UploadPercentage = Math.round(((progressEvent.loaded*100)/progressEvent.total)) + "%"
+            $('#uploadpercentageid').html(UploadPercentage)
+            let UpMB= (progressEvent.loaded/(1024*1024)).toFixed(2) +" MB";
+           let UpPer= ((progressEvent.loaded*100)/progressEvent.total).toFixed(2) +" %";
+
+           $('#uploadMBid').html(UpMB)
+           $('#uploadperMBid').html(UpPer)
+        }
+        }
+
 
         let url = '/fileUp'
 
@@ -42,23 +62,34 @@
         axios.post(url,FileData,config)
         .then(function (response) {
             if(response.status==200){
-                $('#UploadBtnID').html('Upload Success')
+                $('#uploadpercentageid').html('Upload Success')
+                // $('#UploadBtnID').html('Upload Success')
+                // setTimeout(function(){
+                //     $('#UploadBtnID').html('Upload');
+                // }, 3000);
+
                 setTimeout(function(){
-                    $('#UploadBtnID').html('Upload');
-                }, 3000);
+                    $('#uploadpercentageid').html('Upload Success')
+                }, 2000);
 
             }else{
-                $('UploadBtnID').html('Upload Fail')
+                // $('UploadBtnID').html('Upload Fail')
+                // setTimeout(function(){
+                //     $('#UploadBtnID').html('Upload');
+                // }, 3000);
                 setTimeout(function(){
-                    $('#UploadBtnID').html('Upload');
-                }, 3000);
+                    $('#uploadpercentageid').html('Upload failed')
+                }, 2000);
             }
         })
         .catch(function (error) {
-            $('UploadBtnID').html('Upload Fail')
-                setTimeout(function(){
-                    $('#UploadBtnID').html('Upload');
-                }, 3000);
+            // $('UploadBtnID').html('Upload Fail')
+            //     setTimeout(function(){
+            //         $('#UploadBtnID').html('Upload');
+            //     }, 3000);
+            setTimeout(function(){
+                    $('#uploadpercentageid').html('Upload failed')
+                }, 2000);
         })
     }
 </script>
